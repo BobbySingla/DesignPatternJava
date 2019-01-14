5a3537d713abf4be6453094cf7f9c9145c6141e3
Bobby Singla <bobbysingla1112@gmail.com>
2019-01-14 15:10:37 +0530

    DesignPatternsInJava

--- src/main/java/StructuralPattern/CompositePattern/CompositePatternAdmin.java
+++ src/main/java/StructuralPattern/CompositePattern/CompositePatternAdmin.java
@@ -2,24 +2,19 @@ package StructuralPattern.CompositePattern;
 
 public class CompositePatternAdmin {
     public static void main(String[] args) {
-
         Employee CEO = new Employee("Robert", "CEO", 100000);
-        Employee HeadSales = new Employee("Ram", "HeadSales", 60000);
-
+        Employee HeadManager = new Employee("Ram", "HeadManager", 60000);
         Employee Manager = new Employee("Hari", "Manager", 40000);
         Employee Manager1 = new Employee("HariParshad", "Manager", 40000);
         System.out.println(CEO);
-        CEO.add(HeadSales);
-        HeadSales.add(Manager);
-        HeadSales.add(Manager1);
-
+        CEO.add(HeadManager);
+        HeadManager.add(Manager);
+        HeadManager.add(Manager1);
         for (Employee employee : CEO.getSubordinates()) {
-            System.out.println(employee);
-            for (Employee employee1 : HeadSales.getSubordinates()) {
+            for (Employee employee1 : HeadManager.getSubordinates()) {
                 System.out.println(employee1);
             }
+            System.out.println(employee);
         }
-
-
     }
 }
